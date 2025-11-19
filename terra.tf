3b454c678559b38e65d7411b6b031745d62f48a7
bentefranzen <68290020+bentefranzen@users.noreply.github.com>
2025-11-20 00:40:04 +0100

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -1,60 +1,60 @@
-provider "vsphere" {
-  user                 = "administrator@vsphere.local"
-  password             = ""
-  vsphere_server       = "vcsa8.can.cs8.local"
-  allow_unverified_ssl = true
-}
-
-data "vsphere_datacenter" "dc" {
-  name = "AMVLAB"
-}
-
-data "vsphere_datastore" "datastore" {
-  name          = "vsanDatastore"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_compute_cluster" "cluster" {
-  name          = "CL1"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_resource_pool" "pool" {
-  name          = "Resources"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_network" "network" {
-  name          = "dc-mgmt"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-
-data "vsphere_virtual_machine" "template" {
-  name          = "ubuntu-down"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-resource "vsphere_virtual_machine" "vm" {
-  name             = "morpheus-tf-vm"
-  guest_id = "ubuntu64Guest"
-  num_cpus  = 1
-  memory  = 1024
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-network_interface {
-    network_id   = data.vsphere_network.network.id
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+terraform {
+  required_providers {
+    morpheus = {
+      source  = "gomorpheus/morpheus"
+      version = " >= 0.12.0"
+    }
   }
+}
 
- disk {
-    label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks[0].size
-    thin_provisioned = true
-  }
+provider "morpheus" {
+  url          = "https://emorph.can.cs8.local/"
+  access_token = ""
+  secure       = false
+}
 
-    clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
+
+data "morpheus_group" "Group" {
+  name = "VMware Group"
+}
+
+data "morpheus_cloud" "Cloud" {
+  name = "VMware Cloud"
+}
+
+data "morpheus_resource_pool" "Pool" {
+  name    = "CL1"
+  cloud_id = data.morpheus_cloud.Cloud.id
+}
+
+data "morpheus_instance_type" "ubuntu" {
+  name = "Ubuntu"
+}
+
+data "morpheus_instance_layout" "Layout" {
+  name = "VMware VM"
+  version = "24.04"
+}
+
+data "morpheus_network" "vmnetwork" {
+  name = "dc-mgmt"
+}
+
+data "morpheus_plan" "Plan" {
+  name           = "1 CPU, 2GB Memory"
+  provision_type = "vmware"
+}
+
+resource "morpheus_vsphere_instance" "tf_example_instance" {
+  name               = "tfInstance2"
+  description        = "Terraform instance example"
+  cloud_id           = data.morpheus_cloud.Cloud.id
+  group_id           = data.morpheus_group.Group.id
+  instance_type_id   = data.morpheus_instance_type.ubuntu.id
+  instance_layout_id = data.morpheus_instance_layout.Layout.id
+  plan_id            = data.morpheus_plan.Plan.id
+  resource_pool_id   = data.morpheus_resource_pool.Pool.id
+  interfaces {
+    network_id = data.morpheus_network.vmnetwork.id
   }
 }
