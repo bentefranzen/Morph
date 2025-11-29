d7966363891922abed54fffeb3fc9f105defa485
bentefranzen <68290020+bentefranzen@users.noreply.github.com>
2025-11-29 16:24:08 +0100

    Update terra.tf

--- terra.tf
+++ terra.tf
@@ -58,7 +58,7 @@ data "morpheus_plan" "Plan" {
 
 resource "morpheus_vsphere_instance" "tf_example_instance" {
   count              = 2
-  name               = var.morpheus_instance_name "${count.index}"
+  name               = "DemoInstance - ${count.index}"
   description        = "Terraform instance example"
   cloud_id           = data.morpheus_cloud.Cloud.id
   group_id           = data.morpheus_group.Group.id
